ea807e242e0ba6147cff0cf3669e72c8d1fc5825
natashi-sankar <nsankar@emich.edu>
2025-07-10 11:44:20 -0400

    Programmer 2 update

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("Hello there");
+		System.out.println("Programmer 2 did this");
 	}
 
 }
