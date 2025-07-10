2b1fbc6add5a9dd337c9e869936585bd88a20ee2
natashi-sankar <nsankar@emich.edu>
2025-07-10 11:57:04 -0400

    Programer2 altered

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		System.out.println("Hello there");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this, ProgramerOne did this");
 	}
 
 }
