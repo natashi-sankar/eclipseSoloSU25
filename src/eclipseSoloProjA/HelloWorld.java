16d4717bca8c989f35fd065040113bfc37101b12
natashi-sankar <nsankar@emich.edu>
2025-07-10 11:18:32 -0400

    did some changes again

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,7 +3,7 @@ package eclipseSoloProjA;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
-		System.out.println("Hello there!");
+		System.out.println("Hello there");
 	}
 
 }
